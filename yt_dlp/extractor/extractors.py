cb1f7fb5dad443e9d44949af4db39a2527fa063c
Moritz Barsnick <barsnick@gmx.net>
2022-04-30 13:43:35 +0200

    [radioplay] add RadioplayRedirect extractor
    This is a redirect to the other extractors, as used on the
    radioplay front pages.

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -1343,6 +1343,7 @@ from .radiofrance import RadioFranceIE
 from .radioplay import (
     RadioplayIE,
     RadioplayPodcastIE,
+    RadioplayRedirectIE,
 )
 from .radiozet import RadioZetPodcastIE
 from .radiokapital import (
